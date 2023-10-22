535075eb896a8563c95f2110fb2f435f770b95b0
mikalaiserhiyenia <mikalai.serhiyenia@gmail.com>
2023-10-22 16:52:24 +0200

    feat: task solution (Sun, Oct 22, 2023 04:52:24 PM)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const stack = [];
+  const openingBrackets = new Set();
+  const closingBrackets = new Set();
+  const bracketPairs = {};
+  for (const [open, close] of bracketsConfig) {
+    openingBrackets.add(open);
+    closingBrackets.add(close);
+    bracketPairs[close] = open;
+  }
+  for (const bracket of str) {
+    if (openingBrackets.has(bracket)) {
+      if (stack.length > 0 && stack[stack.length - 1] === bracket) {
+        stack.pop();
+      } else {
+        stack.push(bracket);
+      }
+    } else if (closingBrackets.has(bracket)) {
+      if (stack.length === 0 || stack.pop() !== bracketPairs[bracket]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
